cd923ee4f1748a178c56154831e8c5bc2bc3568f
igi <github@6639.33mail.com>
2020-10-31 23:09:46 +0100

    Add get functions

--- calculator.py
+++ calculator.py
@@ -1,131 +1,101 @@
-import sys
-import logging
-logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
+# import logging
+# logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
 
-def substraction():
-    try:
-        first_number = float(input("Proszę podać pierwszą liczbę: "))
-        try:
-            second_number = float(input("Proszę podać drugą liczbę: "))
-            result = first_number - second_number
-            logging.info(f"Odejmuję {second_number} od {first_number}")
-            print(f"Wynik działania to {result}")
-        except:
-            print("\nProszę podać LICZBĘ\n")
-            substraction()
-    except ValueError:
-        print("\nProszę podać LICZBĘ\n")
-        substraction()
+# def substraction(x, y):
+#     return x, y
 
 
-def division():
-    try:
-        first_number = float(input("Proszę podać pierwszą liczbę: "))
-        try:
-            second_number = float(input("Proszę podać drugą liczbę: "))
-            if second_number != 0:
-                result = first_number / second_number
-                logging.info(f"Dzielę {first_number} przez {second_number}")
-                print(f"Wynik działania to {result}")
-            else:
-                print("Pamiętaj cholero, nie dziel przez zero!")
-        except ValueError:
-            print("\nProszę podać LICZBĘ\n")
-            division()
-    except ValueError:
-        print("\nProszę podać LICZBĘ\n")
-        division()
+# def division(x, y):
+#     if y != 0:
+#         return x / y
+#     else:
+#         print("Pamiętaj cholero, nie dziel przez zero!")
 
 
-def addition():
-    try:
-        number_of_items = int(input("Ile liczb chciałbyś dodać? "))
-        items_list = []
-        sum_of_items = 0
-        correct_numbers = True
-        for i in range(number_of_items):
-            number = input(f"Proszę podać liczbę numer {i + 1}: ")    
-            items_list.append(number)
-        
-        for item in items_list:
-            try:
-                sum_of_items += float(item)
-            except ValueError:
-                correct_numbers = False
-                sum_of_items = 0
-                print("\n Podano niewłaściwą liczbę\n")
-        
-        if correct_numbers == True:  
-            items_string = ', '.join(item for item in items_list)
-            logging.info(f"Dodaję następujące liczby: {items_string}")
-            print(f"Wynik działania to {sum_of_items}")
-        else:
-            addition()
+# def addition(x, y, *args):
+#     return x + y + sum(args)
 
-    except ValueError:
-        print("\nProszę wybrać LICZBĘ\n")
-        addition()
    
+# def multiplication(x, y, *args):
+#     multi = 1
+#     for number in args:
+#         multi *= number
+#     return x * y * multi
 
-def multiplication():
-    try:
-        number_of_items = int(input("Ile liczb chciałbyś pomnożyć? "))
-        items_list = []
-        multi_items = 1
-        correct_numbers = True
-        for i in range(number_of_items):
-            number = input(f"Proszę podać liczbę numer {i + 1}: ")
-            items_list.append(number)
 
-        for item in items_list:
-            try:
-                multi_items *= float(item)
-            except ValueError:
-                correct_numbers = False
-                multi_items = 1
-                print("\n Podano niewłaściwą liczbę\n")
-
-        if correct_numbers == True:  
-            items_string = ', '.join(item for item in items_list)
-            logging.info(f"Mnożę następujące liczby: {items_string}")
-            print(f"Wynik działania to {multi_items}")
-        else:
-            multiplication()
+# def get_data(a: int):
+#     if a == 1:
+#         x = float(input("Proszę podaj odjemną: "))
+#         y = float(input("Proszę podaj odjemnik: "))
+#         return x, y
+#     elif a == 2:
+#         x = float(input("Proszę podaj dzielną: "))
+#         y = float(input("Proszę podaj dzielnik: "))
+#         return x, y
+#     elif a == 3:
+#         numbers_to_add = []
+#         comp_number = int(input("Ile składników chciałbyś dodać? "))
+#         for i in range(comp_number):
+#             print(f"Proszę podaj składnik {i + 1}: ")
+#             number = float(input())
+#             numbers_to_add.append(number)
         
-    except ValueError:
-        print("\nProszę wybrać LICZBĘ\n")
-        multiplication()
+#         x = numbers_to_add[0]
+#         y = numbers_to_add[1]
+#         args = numbers_to_add[2:]
+#         return x, y, args
+#     elif a == 4:
+#         numbers_to_multi = []
+#         comp_number = int(input("Ile czynników chciałbyś pomnożyć? "))
+#         for i in range(comp_number):
+#             print(f"Proszę podaj czynnik {i + 1}: ")
+#             number = float(input())
+#             numbers_to_multi.append(number)
+        
+#         x = numbers_to_multi[0]
+#         y = numbers_to_multi[1]
+#         args = numbers_to_multi[2:]
+#         return x, y, args     
 
-def choosing():
 
-    time_to_choose = """
-    Witamy w kalkulatorze!
-    Jakie działanie chciałbyś wykonać? Podaj odpowiednią liczbę:
-    1. Odejmowanie
-    2. Dzielenie
-    3. Dodawanie
-    4. Mnożenie
-    (Naciśnij 0 aby wyjść)
-    """
-    print(time_to_choose)
-    try:
-        operation = int(input())
-        if operation == 1:
-            substraction()        
-        elif operation == 2:
-            division()
-        elif operation == 3:
-            addition()
-        elif operation == 4:
-            multiplication()
-        elif operation == 0:
-            print("Koniec gry!")
-            exit(1)
-        else:
-            print("Proszę wybrać odpowiedni numer")
-    except ValueError:
-        print("Proszę wybrać NUMER")
+# def choosing():
 
-while True:
-    choosing()
+#     time_to_choose = """
+#     Witamy w kalkulatorze!
+#     Jakie działanie chciałbyś wykonać? Podaj odpowiednią liczbę:
+#     1. Odejmowanie
+#     2. Dzielenie
+#     3. Dodawanie
+#     4. Mnożenie
+#     (Naciśnij 0 aby wyjść)
+#     """
+#     print(time_to_choose)
+#     try:
+#         operation = int(input())
+#         if operation == 1:
+#             x, y, args = get_data(1)
+#             result = substraction(x, y)
+#             print(f"Wynik odejmowania to {result}")       
+#         elif operation == 2:
+#             x, y, args = get_data(2)
+#             result = division(x, y)
+#             print(f"Wynik dzielenia to {result}")
+#         elif operation == 3:
+#             x, y, args = get_data(3)
+#             result = addition(x, y, args)
+#             print(f"Wynik dodawania to {result}")
+#         elif operation == 4:
+#             x, y, args = get_data(4)
+#             result = multiplication(x, y, args)
+#             print(f"Wynik mnożenia to {result}")
+#         elif operation == 0:
+#             print("Koniec gry!")
+#             exit(1)
+#         else:
+#             print("Proszę wybrać odpowiedni numer")
+#     except ValueError:
+#         print("Proszę wybrać NUMER")
+
+# while True:
+#     choosing()
